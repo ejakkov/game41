acde9ebd36870b681119ac9eb4f7ee251b383ac1
ejakkov <139910297+ejakkov@users.noreply.github.com>
2024-04-05 09:53:30 +0300

    Update game41.py

--- game41.py
+++ game41.py
@@ -273,28 +273,6 @@ class minmax_game:
         if ccw((x1, y1), (x3, y3), (x4, y4)) != ccw((x2, y2), (x3, y3), (x4, y4)) and ccw((x1, y1), (x2, y2),(x3, y3)) != ccw((x1, y1),(x2, y2),(x4, y4)):
             return True
         
-    def draw_line(self,index_button1,index_button2):
-        x1, y1 = self.button_coords[index_button1]
-        x2, y2 = self.button_coords[index_button2]
-        line=self.canvas.create_line(x1,y1,x2,y2,fill="black")
-        self.drawn_lines.append(line)
-
-    def save_line(self, index_button1, index_button2):
-        x1, y1 = self.button_coords[index_button1]
-        x2, y2 = self.button_coords[index_button2]
-        line = (x1, y1, x2, y2)
-        self.lines.append(line)
-
-    def check_intersections(self,line1,line2):
-        x1,y1,x2,y2=line1
-        x3,y3,x4,y4=line2
-        cross1=(x4-x3)*(y1-y3)-(y4-y3)*(x1-x3)
-        cross2=(x4-x3)*(y2-y3)-(y4-y3)*(x2-x3)
-        cross3=(x2-x1)*(y3-y1)-(y2-y1)*(x3-x1)
-        cross4=(x2-x1)*(y4-y1)-(y2-y1)*(x4-x1)
-        if cross1*cross2<0 and cross3*cross4<0:
-            return True
-        return False
         
     def check_player(self):
         line1 = self.lines[-1]
